7f546ec7beb7d3e1593ae3a8c6ed4dfa4178d6da
HenriqueForte <a32771@tomazpelayo.com>
2023-01-23 14:31:46 +0000

    Update OlaaCores.c

--- OlaaCores.c
+++ OlaaCores.c
@@ -1,3 +1,14 @@
 // Resolva neste ficheiro o seu exercício
 
-
+#include <stdio.h> 
+ #include <windows.h> 
+ int main() { 
+    HANDLE hConsole = GetStdHandle (STD_OUTPUT_HANDLE); 
+    SetConsoleTextAttribute (hConsole, FOREGROUND_RED); 
+    printf("Ola Mundo!\n"); 
+    SetConsoleTextAttribute (hConsole, FOREGROUND_GREEN);
+     printf("Ola Mundo!\n");
+      SetConsoleTextAttribute (hConsole, FOREGROUND_BLUE); 
+      printf("Ola Mundo!\n");
+      
+    }
